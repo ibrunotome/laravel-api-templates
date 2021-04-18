ad74285922a6a76514ce606911a76e7e1b93a24a
Bruno Tomé <ibrunotome@gmail.com>
2021-04-18 09:50:59 -0300

    ci: improve Architecture score

--- modular-structure/config/insights.php
+++ modular-structure/config/insights.php
@@ -79,6 +79,7 @@ return [
     'remove' => [
         AlphabeticallySortedUsesSniff::class,
         ArrayIndentSniff::class,
+        ClassDeclaration::class,
         DeclareStrictTypesSniff::class,
         DisallowMixedTypeHintSniff::class,
         DisallowEmptySniff::class,
